5592c64107ffc64ec356e2fb50cd1d989d7e25ba
Richard You <richardyou94@gmail.com>
2023-02-14 22:14:44 -0800

    insert many function work

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -1,9 +1,10 @@
 const { MongoClient } = require("mongodb");
 // const assert = require("assert");
 
-const uri = "mongodb://localhost:27017";
+const uri =
+  "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0";
 
-const dbName = "myproject";
+const dbName = "dbFruits";
 
 const client = new MongoClient(uri);
 
@@ -21,8 +22,30 @@ run().catch(console.dir);
 
 const myDB = client.db(dbName);
 const myColl = myDB.collection("fruits");
-const doc = { name: "Neapolitan pizza", shape: "round" };
-const result = await myColl.insertOne(doc);
-console.log(
-   `A document was inserted with the _id: ${result.insertedId}`,
-);
+
+async function insertMany() {
+  try {
+    const docs = [
+      { _id: 1, fruit: "Apple", score: 8, review: "Great fruit" },
+      { _id: 2, fruit: "Orange", score: 6, review: "Kinda sour" },
+      { _id: 3, fruit: "Banana", score: 9, review: "Great stuff!" },
+    ];
+    const insertManyResult = await myColl.insertMany(docs);
+    let ids = insertManyResult.insertedIds;
+
+    console.log(`${insertManyResult.insertedCount} documents were inserted.`);
+    for (let id of Object.values(ids)) {
+      console.log(`Inserted a document with id ${id}`);
+    }
+  } catch (e) {
+    console.log(
+      `A MongoBulkWriteException occurred, but there are successfully processed documents.`
+    );
+    let ids = e.result.result.insertedIds;
+    for (let id of Object.values(ids)) {
+      console.log(`Processed a document with id ${id._id}`);
+    }
+    console.log(`Number of documents inserted: ${e.result.result.nInserted}`);
+  }
+}
+insertMany();
