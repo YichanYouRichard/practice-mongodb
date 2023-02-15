27e8bae405ac9c19a8a059df4d9aa93598bd1e59
Richard You <richardyou94@gmail.com>
2023-02-15 12:07:51 -0800

    have both fruits and person

--- usingMongoose/app.js
+++ usingMongoose/app.js
@@ -1,21 +1,35 @@
 const mongoose = require("mongoose");
 
-const url = "mongodb://localhost:27017/peopleDB";
+const url = "mongodb://localhost:27017/fruitsDB";
 
 mongoose.set("strictQuery", false);
 
 mongoose.connect(url, { useNewUrlParser: true });
+
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String,
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 7,
+  review: "Pretty solid as a fruit.",
+});
+
+// fruit.save();
 
 const personSchema = new mongoose.Schema({
   name: String,
   age: Number,
 });
 
-const Person = mongoose.model("person", personSchema);
+const Person = mongoose.model("Person", personSchema);
 
 const person = new Person({
   name: "John",
   rating: 31,
 });
-
-person.save();
