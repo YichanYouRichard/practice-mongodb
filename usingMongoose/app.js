5abfada506e7ff35ea022893516155902fdbd9c3
Richard You <richardyou94@gmail.com>
2023-02-15 12:00:54 -0800

    able to add fruit data using model schema and into database

--- usingMongoose/app.js
+++ usingMongoose/app.js
@@ -12,29 +12,10 @@ const fruitSchema = new mongoose.Schema({
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
-async function insertMany() {
-  try {
-    const docs = [
-      { _id: 1, fruit: "Apple", score: 8, review: "Great fruit" },
-      { _id: 2, fruit: "Orange", score: 6, review: "Kinda sour" },
-      { _id: 3, fruit: "Banana", score: 9, review: "Great stuff!" },
-    ];
-    const insertManyResult = await myColl.insertMany(docs);
-    let ids = insertManyResult.insertedIds;
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 7,
+  review: "Pretty solid as a fruit.",
+});
 
-    console.log(`${insertManyResult.insertedCount} documents were inserted.`);
-    for (let id of Object.values(ids)) {
-      console.log(`Inserted a document with id ${id}`);
-    }
-  } catch (e) {
-    console.log(
-      `A MongoBulkWriteException occurred, but there are successfully processed documents.`
-    );
-    let ids = e.result.result.insertedIds;
-    for (let id of Object.values(ids)) {
-      console.log(`Processed a document with id ${id._id}`);
-    }
-    console.log(`Number of documents inserted: ${e.result.result.nInserted}`);
-  }
-}
-insertMany();
+fruit.save();
